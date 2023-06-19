49e4c87b20429ad3b1cb6106b56e7097f44a44a6
Immolin <serebrena@mail.ru>
2023-06-19 21:08:14 +1000

    Нужно больше функций

---
+++ js/functions.js
@@ -0,0 +1,24 @@
+function checkString (string, maxLength) {
+  if (string.length <= maxLength) {
+    return true;
+  }
+  return false;
+}
+
+function checkPalindrome (string) {
+  const newString = string.toLowerCase().replaceAll(' ', '');
+  let emptyString = '';
+  for (let i = newString.length - 1; i >= 0; --i) {
+    emptyString += newString[i];
+  }
+  return newString === emptyString;
+}
+
+function getNumber (string) {
+  const modifiedString = string.toString().replaceAll(/\D/g, '');
+  if (modifiedString.length === 0) {
+    return NaN;
+  }
+  return Number(modifiedString);
+}
+
